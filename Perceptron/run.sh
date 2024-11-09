aeb87f88423cb08a4f04255d89a4c122672b372b
RakibulHaqueSajal <rakibulhaq56@gmail.com>
2024-11-08 18:41:00 -0700

    bash file added

---
+++ Perceptron/run.sh
@@ -0,0 +1,7 @@
+echo 'Running Perceptrons'
+echo 'Running Standard Perceptron'
+python standard_perceptron.py
+echo 'Running Voted Percpetron'
+python voted_perceptron.py
+echo 'Running Average_Perceptron'
+python average_perceptron.py
